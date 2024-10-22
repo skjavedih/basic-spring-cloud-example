9d73971f6b5aa273b97c9a5e6852c1e5ecd26622
Javed Shaikh <shaikhjavedmail@gmail.com>
2024-10-22 21:30:16 +0530

    Initial commit

---
+++ start/greeting/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Javed
+ */
+@RestController
+public class GreetingController {
+	private static final Logger LOG = Logger.getLogger(GreetingController.class.getName());
+
+	private GreetingProperties greetingProperties;
+
+	public GreetingController(GreetingProperties greetingProperties) {
+		this.greetingProperties = greetingProperties;
+	}
+
+	@RequestMapping("/{languageCode}")
+	public String getGreeting(@PathVariable String languageCode){
+		LOG.info("Language Code: " + languageCode);
+		LOG.info("Greeting: " + greetingProperties.getGreetings().get(languageCode.toUpperCase()));
+		if(!languageCode.isEmpty()){
+			return greetingProperties.getGreetings().get(languageCode.toUpperCase());
+		} else {
+			return greetingProperties.getGreetings().get("EN");
+		}
+		//return greetingProperties.getGreetings().getOrDefault(languageCode.toUpperCase(), greetingProperties.getGreeting());
+	}
+
+	@RequestMapping("/")
+	public String getGreeting(){
+		LOG.info("Greeting: " + greetingProperties.getGreeting());
+		return greetingProperties.getGreeting();
+	}
+}
---
+++ start/name/src/main/java/com/example/demo/NameController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Javed
+ */
+@RestController
+public class NameController {
+	private static final Logger LOG = Logger.getLogger(NameController.class.getName());
+	private NameProperties nameProperties;
+	public NameController(NameProperties nameProperties) {
+		this.nameProperties = nameProperties;
+	}
+
+	@RequestMapping
+	public String getName() {
+		LOG.info("Name: " + nameProperties.getName());
+		return nameProperties.getName();
+	}
+}
